3b902c9e96d04c76e45ef02c479793c47ae2e97b
aliefabdussalam <83624142+aliefabdussalam@users.noreply.github.com>
2021-11-05 07:25:53 +0700

    revisi

--- week10/task/function.js
+++ week10/task/function.js
@@ -3,7 +3,7 @@ data.forEach((e, i) => {
     <div class="div" id="${e.id}">
       <div class="img-prd" onclick="add(${e.id})"><img src="${e.picture}" height="150px"></div>
       <div class="product_name">${e.product_name}</div>
-      <div class="priceStyle">${e.priceStyle}</div>
+      <div class="priceStyle">${e.price}</div>
     </div>`
    
   });
@@ -28,6 +28,8 @@ const add = (id) => {
   getTocart()
 }
 const getTocart = () => {
+  
+  document.getElementById("chart").innerHTML =''
   chart.forEach((e, i) => {
     document.getElementById("chart").innerHTML += `
     <div class="cart-item" id="${e.id}">
@@ -35,33 +37,72 @@ const getTocart = () => {
       <div class="desc">
         <div class="product_name-cart">${e.product_name}</div>
         <div class="quantity">
-          <button class="min" onclick="decrement(${e.id})">-</button>
+          <button class="min" onclick="min(${e.id})">-</button>
           <div class="qty">${e.qty}</div>
           <button class="plus" onclick="add(${e.id})">+</button>
           <div class="price">${e.price * e.qty}</div>
         </div>
       </div>
     </div>`
-  }); 
+  })
+  if (chart.length > 0){
+    let total=0
+    chart.forEach ((e)=>{
+        total += e.price * e.qty
+        document.getElementById("total").innerHTML = `
+        <div class="position-fixed bottom-0">
+            <div class="row ms-2">
+                <div class="col-6 fw-bold price"> Total</div>
+                <div class="col-6 d-flex justify-content-end fw-bold pe-3 price">${total}</div>
+                <p class="fw-normal fs-15 mb-2 mt-1">*Belum Termasuk ppn</p>
+            </div>
+            <div class="d-flex flex-column checkout ms-2">
+                <button onclick="" class="mb-3 p-2 blue "data-bs-toggle="modal" data-bs-target="#exampleModal">CheckOut</button>
+                <button onclick="selected()" class="p-2 mb-3 pink">Cancel</button>
+            </div>
+            
+        </div>`
+    })
+}else {
+    document.getElementById("total").innerHTML = `
+        <div class="d-flex mt-4 flex-column align-items-center">
+        <img src="./photo/food-and-restaurant.png" width="150px" height="auto" >
+        <h1 class="TittleEmpty fw-normal mt-2 mb-0">Your Cart is Empty</h1>
+        <p class="Textempty"> Please add some items from the menu</p>
+    </div>`
+}; 
+  
 }
-let chartobj = chart[length]
+
 
 const selected = () =>{
-  document.getElementById("chart").innerHTML =''
+  total = 0
+  chart = []
+ getTocart()
+ 
 }
 const decrement = (id) =>{
-    const newcart = chart.map((e) =>{
-        if (e.id === id) {
-          return {
-            id: e.id,
-            name: e.product_name,
-            price : e.price,
-            qty: e.qty - 1
-          }
-        }else{
-          return e
-        }
-        
-    })
-    chart = newcart
+ 
+    same = chart.findIndex((dec => dec.id === id))
+    if (chart[same].qty <= 1) {
+      chart.splice([same],1)
+    }else{
+      chart[same].qty  -= 1
+    }
+  }
+  const min = (id) =>{
+    decrement(id)
+    getTocart()
+    total()
 }
+
+// const getEmpty = () =>{
+ 
+//     document.getElementById('cartEmpty').innerHTML =`
+//     <img class="empty-img" src="./photo/food-and-restaurant.png" alt="">
+//           <h4 class="empty-text">Your cart is empty</h4>
+//           <p class="empty-text1">Please add some items from the menu</p>`
+    
+
+// }
+// getEmpty()
